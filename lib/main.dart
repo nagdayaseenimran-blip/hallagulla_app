5637141163f5f76e0c0ff626f72d5f68c744eb19
nagdayaseenimran-blip <nagdayaseenimran@gmail.com>
2026-08-18 03:40:04 +0500

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,248 @@
+import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:chewie/chewie.dart';
+
+void main() => runApp(const NetflixMediaApp());
+
+class NetflixMediaApp extends StatelessWidget {
+  const NetflixMediaApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'HallaGulla Prime',
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: const Color(0xFF000000),
+        primaryColor: Colors.redAccent,
+      ),
+      home: const MainTabScreen(),
+    );
+  }
+}
+
+class MainTabScreen extends StatefulWidget {
+  const MainTabScreen({super.key});
+
+  @override
+  State<MainTabScreen> createState() => _MainTabScreenState();
+}
+
+class _MainTabScreenState extends State<MainTabScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = const [
+    MediaCategoryPage(categoryTitle: 'Movies'),
+    MediaCategoryPage(categoryTitle: 'Music'),
+    MediaCategoryPage(categoryTitle: 'Web TV'),
+    MediaCategoryPage(categoryTitle: 'Videos'),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index) => setState(() => _currentIndex = index),
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: const Color(0xFF121212),
+        selectedItemColor: Colors.redAccent,
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.movie), label: 'Movies'),
+          BottomNavigationBarItem(icon: Icon(Icons.music_note), label: 'Music'),
+          BottomNavigationBarItem(icon: Icon(Icons.live_tv), label: 'Web TV'),
+          BottomNavigationBarItem(icon: Icon(Icons.video_library), label: 'Videos'),
+        ],
+      ),
+    );
+  }
+}
+
+class MediaCategoryPage extends StatelessWidget {
+  final String categoryTitle;
+  const MediaCategoryPage({super.key, required this.categoryTitle});
+
+  // Sample Direct MP4 Video URL
+  final String sampleVideoUrl =
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/butterfly.mp4';
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAlignment.start,
+        children: [
+          // Featured Banner
+          Stack(
+            children: [
+              Container(
+                height: 400,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(
+                        'https://images.unsplash.com/photo-1574375927938-d5a98e8ffe85?w=800'),
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              Container(
+                height: 400,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [Colors.black, Colors.transparent, Colors.black],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CustomVideoPlayer(
+                              videoUrl: sampleVideoUrl,
+                              title: '$categoryTitle Featured',
+                            ),
+                          ),
+                        );
+                      },
+                      icon: const Icon(Icons.play_arrow, color: Colors.black),
+                      label: const Text('Play Now',
+                          style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold)),
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+
+          // Horizontal Rows
+          _buildRow(context, 'Trending $categoryTitle'),
+          _buildRow(context, 'Popular Releases'),
+          _buildRow(context, 'Top Rated'),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRow(BuildContext context, String title) {
+    return Column(
+      crossAxisAlignment: CrossAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Text(title,
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+        ),
+        SizedBox(
+          height: 160,
+          child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: 6,
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => CustomVideoPlayer(
+                        videoUrl: sampleVideoUrl,
+                        title: '$title #${index + 1}',
+                      ),
+                    ),
+                  );
+                },
+                child: Container(
+                  width: 110,
+                  margin: const EdgeInsets.only(left: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(8),
+                    image: const DecorationImage(
+                      image: NetworkImage(
+                          'https://images.unsplash.com/photo-1536440136628-849c177e76a1?w=400'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+// Native Video Player Screen
+class CustomVideoPlayer extends StatefulWidget {
+  final String videoUrl;
+  final String title;
+
+  const CustomVideoPlayer({super.key, required this.videoUrl, required this.title});
+
+  @override
+  State<CustomVideoPlayer> createState() => _CustomVideoPlayerState();
+}
+
+class _CustomVideoPlayerState extends State<CustomVideoPlayer> {
+  late VideoPlayerController _videoPlayerController;
+  ChewieController? _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializePlayer();
+  }
+
+  Future<void> _initializePlayer() async {
+    _videoPlayerController =
+        VideoPlayerController.networkUrl(Uri.parse(widget.videoUrl));
+    await _videoPlayerController.initialize();
+
+    _chewieController = ChewieController(
+      videoPlayerController: _videoPlayerController,
+      autoPlay: true,
+      looping: false,
+      aspectRatio: _videoPlayerController.value.aspectRatio,
+    );
+
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    _chewieController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.black,
+      ),
+      body: Center(
+        child: _chewieController != null &&
+                _chewieController!.videoPlayerController.value.isInitialized
+            ? Chewie(controller: _chewieController!)
+            : const CircularProgressIndicator(color: Colors.redAccent),
+      ),
+    );
+  }
+}
